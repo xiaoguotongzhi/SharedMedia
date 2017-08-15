2538fa2455020f9373f84ca111bc54fc20bcd4c3
guo <guoguo@qq.com>
2017-08-15 16:55:50 +0800

    修改了index控制器web访问的bug

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -5,6 +5,10 @@ use Think\Controller;
 use Common\Common\Helper;
 use Common\Common\Status;
 header("Content-type: application/json; charset=utf-8");
+header('Access-Control-Allow-origin:*');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers:Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
 class IndexController extends Controller{
     public function index(){
         return Helper::response(Status::FAIL);
