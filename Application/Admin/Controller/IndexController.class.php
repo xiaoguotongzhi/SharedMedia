e92621fe88fd29965436f4e18c80ee9e8d503852
guo <guoguo@qq.com>
2017-08-24 09:47:33 +0800

    登录接口的返回多加了一个username

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -33,7 +33,7 @@ class IndexController extends Controller{
             if($adminUser->where('id='.$userInfo['id'])->save($data)){
                 $rs = $user->where('id='.$userInfo['id'])->find();
                 session('username',$username);
-                return Helper::response(Status::SUCCESS,['user_id'=>$rs['id'],'token'=>$rs['token']]);
+                return Helper::response(Status::SUCCESS,['user_id'=>$rs['id'],'token'=>$rs['token'],'username'=>$username]);
             }
         }
     }
